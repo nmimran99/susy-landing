4b7a8ef5443ef56308a9e35c71a15929777c8cf7
Niv Mimran <nmimran99@gmail.com>
2024-07-25 16:15:08 +0200

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
 import Navbar from './compnents/Navbar'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google' 
 
 export const metadata = {
   title: 'Medicina con Susy',
@@ -14,19 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="he" dir="rtl">
-      <Head>
-      <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          }(window,document,'script','dataLayer','GTM-KCD7KWQC');
-          `,
-          }}>
-        </script>
-      </Head>
+      <GoogleTagManager gtmId="'GTM-KCD7KWQC" />
       <body>
         <Navbar />
         {children}
