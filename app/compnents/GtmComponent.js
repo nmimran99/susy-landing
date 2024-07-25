7146b62a529de18d485c23518c8df8393cfcfc29
Niv Mimran <nmimran99@gmail.com>
2024-07-25 16:56:11 +0200

    env

--- app/compnents/GtmComponent.js
+++ app/compnents/GtmComponent.js
@@ -22,7 +22,7 @@ export default function Analytics() {
     <>
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=GTM-KCD7KWQC`}
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
@@ -37,7 +37,7 @@ export default function Analytics() {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', "GTM-KCD7KWQC");
+    })(window,document,'script','dataLayer', "${process.env.NEXT_PUBLIC_GTM_ID}");
   `,
         }}
       />
